84691bb6d9f996e73f315938000f2f6b9a34974d
ADMIN <ADMIN@LAPTOP-72E84BVK>
2024-03-12 19:32:39 +0700

    Add project files.

---
+++ XTBarber/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XTBarber.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult dichvu()
+        {
+            return View();
+
+        }
+
+        public ActionResult gioithieu()
+        {
+            return View();
+        }
+
+        public ActionResult lienhe() {
+
+            return View();
+        }
+
+        public ActionResult thanhtoan()
+        {
+            return View();
+        }
+    }
+}
